caf8c5b8ae3c471e592a5b8098f04aace999dc7e
Asura0107 <mircoorosio@gmail.com>
2024-02-16 10:58:02 +0100

    Initial commit

---
+++ src/test/java/u5d10/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package u5d10.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
